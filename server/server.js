a94959786aeaf35eb3ed9bc26501f3aff1f9b841
Gloria Vela <gloria.laura.vela@gmail.com>
2020-03-20 10:13:26 -0500

    Model observers

--- server/server.js
+++ server/server.js
@@ -10,9 +10,9 @@ const boot = require('loopback-boot');
 
 const app = module.exports = loopback();
 
-app.start = function() {
+app.start = function () {
   // start the web server
-  return app.listen(function() {
+  return app.listen(function () {
     app.emit('started');
     const baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
@@ -25,10 +25,30 @@ app.start = function() {
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
+boot(app, __dirname, function (err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
   if (require.main === module)
     app.start();
+});
+
+// Display the models available
+console.log(Object.keys(app.models));
+
+app.models.User.afterRemote('create', (ctx, user, next) => {
+  console.log('New User is ', user);
+  app.models.Profile.create({
+    first_name: user.username,
+    created_at: new Date(),
+    userId: user.id,
+  }, (err, result) => {
+    if (!err && result) {
+      console.log('Created new profile!', result);
+    }
+    else {
+      console.log('There is an error', err);
+    }
+    next();
+  });
 });
