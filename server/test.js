801db95dd243835e3fbcb61ffc45b7829bb7cd40
Gloria Vela <gloria.laura.vela@gmail.com>
2020-03-26 10:51:22 -0500

    Updating data

--- server/test.js
+++ server/test.js
@@ -8,6 +8,24 @@ var models = require('./server.js').models;
 //   console.log('data?', err, profile);
 // });
 
-models.Profile.findOrCreate({id: '5e78d8cd7a08253bb867242a'}, {name: 'Nick'}, (err, profile) => {
-  console.log('data?', err, profile);
-});
+// models.Profile.upsert({id: '5e7cc7a17b267109c0995801', name: 'Nick2'}, (err, profile) => {
+//   console.log('data?', err, profile);
+// });
+
+// models.Profile.findOrCreate({name: 'Nick2'}, (err, profile) => {
+//   console.log('data?', err, profile);
+//   if (err) {
+//     console.log('There was an error', err);
+//   } else if (profile) {
+//     // add an attribute to the profile - a
+//     // profile.updateAttributes({
+//     //   email: 'nick@nickgermain.com',
+//     // }, (updateError, updated) => {
+//     //   console.log('Saved?', updateError, updated);
+//     // });
+//     profile.email= 'nick@nickgermaine2.com';
+//     profile.save((ue, updated) => {
+//       console.log('Updated?', updated);
+//     });
+//   }
+// });
