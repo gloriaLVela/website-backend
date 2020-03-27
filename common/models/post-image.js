723049dcf2922cf7718fd7d10758ed83b4174e4f
Gloria Vela <gloria.laura.vela@gmail.com>
2020-03-27 14:17:23 -0500

    Custom remote methods and file uploads part 1

--- common/models/post-image.js
+++ common/models/post-image.js
@@ -1,5 +1,31 @@
 'use strict';
+const sharp = require('sharp');
+const fs = require('fs');
 
-module.exports = function(Postimage) {
+module.exports = function(PostImage) {
+  PostImage.upload = function(ctx, options, access_token, post_id, cb) {
+    if (!options) options = {};
+    ctx.request.params.container = 'postImages';
+    if (!fs.existsSync('./server/storage/' + ctx.req.params.container)) {
+      fs.mkdirSync('./server/storage' + ctx.req.params.container);
+    }
+  };
+    // Configure the route
+  PostImage.remoteMethod(
+        'upload',
+    {
+      description: 'uploads a file',
+      accepts: [
+                {args: 'ctx', type: 'object', http: {source: 'context'}},
+                {arg: 'options', type: 'object', http: {source: 'query'}},
+                {arg: 'access_token', type: 'string'},
+                {arg: 'post_id', type: 'string'},
+      ],
+      returns: {
+        arg: 'fileObject', type: 'object', root: true,
+      },
+      http: {verb: 'post'},
+    }
 
+    );
 };
