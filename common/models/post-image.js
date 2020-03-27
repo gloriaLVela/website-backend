9b1de821b6b0be9400ef0056b52a580ff5518909
Gloria Vela <gloria.laura.vela@gmail.com>
2020-03-27 14:51:43 -0500

    Custom remote methids and file uploads part 2

--- common/models/post-image.js
+++ common/models/post-image.js
@@ -1,14 +1,45 @@
+/* eslint-disable camelcase */
+/* eslint-disable max-len */
 'use strict';
 const sharp = require('sharp');
 const fs = require('fs');
 
+const CONTAINER_URL = '/api/containers/';
+
 module.exports = function(PostImage) {
-  PostImage.upload = function(ctx, options, access_token, post_id, cb) {
+  PostImage.upload = function(ctx, options, access_token, post_id, user_id, cb) {
     if (!options) options = {};
     ctx.request.params.container = 'postImages';
     if (!fs.existsSync('./server/storage/' + ctx.req.params.container)) {
       fs.mkdirSync('./server/storage' + ctx.req.params.container);
     }
+
+    PostImage.app.models.ImageFile.upload(ctx.req, ctx.reult, options, (err, file) => {
+      if (err) {
+        cb(err);
+      } else {
+        var fileInfo = file.files.file[0];
+        sharp('./server/storage/' + ctx.req.params.container + '/' + fileInfo.name)
+        .resize(100)
+        .toFile('./server/storage/' + ctx.req.params.container + '/100-' + fileInfo.name, (err) => {
+          if (!err) {
+            PostImage.create({
+              url: CONTAINER_URL + fileInfo.container + '/download/' + fileInfo.name,
+              thumbnail: CONTAINER_URL + fileInfo.container + '/download/100-' + fileInfo.name,
+              created_at: new Date(),
+              postId: post_id,
+              userId: user_id,
+            }, (err2, image) => {
+              if (err2) {
+                cb(err2);
+              } else {
+                cb(null, image);
+              }
+            });
+          }
+        });
+      }
+    });
   };
     // Configure the route
   PostImage.remoteMethod(
@@ -20,6 +51,7 @@ module.exports = function(PostImage) {
                 {arg: 'options', type: 'object', http: {source: 'query'}},
                 {arg: 'access_token', type: 'string'},
                 {arg: 'post_id', type: 'string'},
+                {arg: 'user_id', type: 'string'},
       ],
       returns: {
         arg: 'fileObject', type: 'object', root: true,
